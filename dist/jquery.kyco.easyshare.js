9367a06c737d50f9e7ac165c58397cf0ef21f8b8
Cornelius <cornelius@kyco.co.za>
2014-10-15 23:16:42 +0200

    Initial commit.

---
+++ dist/jquery.kyco.easyshare.js
@@ -0,0 +1,88 @@
+/***************************************\
+
+	jquery.kyco.easyshare
+	=====================
+
+	Version 1.0.0
+
+	Brought to you by
+	http://www.kycosoftware.com
+
+	Copyright 2014 Cornelius Weidmann
+
+	Distributed under the GPL
+
+\***************************************/
+
+var kyco = kyco || {};
+
+kyco.easyShare = function() {
+	if ($('[data-easyshare]').length > 0) {
+		$('[data-easyshare]').each(function(index, element) {
+			var _this     = $(this);
+			var url       = _this.data('easyshare-url');
+			var SHARE_URL = typeof url === 'undefined' || url === '' ? window.location.href : url;
+
+			// Get share counts for Facebook, Twitter and Google+
+			$.ajax({
+				url: 'http://www.kycosoftware.com/api/easyshare',
+				type: 'GET',
+				data: {
+					url: SHARE_URL
+				},
+				dataType: 'json',
+				success: function(response) {
+					_this.find('[data-easyshare-total-count]').html(response.ZeTotal);
+
+					_this.find('[data-easyshare-button-count="facebook"]').html(response.Facebook);
+					_this.find('[data-easyshare-button-count="twitter"]').html(response.Twitter);
+					_this.find('[data-easyshare-button-count="google"]').html(response.Google);
+
+					_this.find('[data-easyshare-loader]').fadeOut(500);
+				}
+			});
+
+			// Facebook share button
+			_this.find('[data-easyshare-button="facebook"]').click(function() {
+				var width  = 500;
+				var height = 400;
+				var left   = ($(window).width() - width) / 2;
+				var top    = ($(window).height() - height) / 2;
+
+				var url    = 'https://www.facebook.com/sharer/sharer.php?u=' + SHARE_URL;
+				var opts   = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
+
+				window.open(url, 'google+', opts);
+			});
+
+			// Twitter share button
+			_this.find('[data-easyshare-button="twitter"]').click(function() {
+				var text   = $(this).data('easyshare-tweet-text');
+				var width  = 575;
+				var height = 440;
+				var left   = ($(window).width() - width) / 2;
+				var top    = ($(window).height() - height) / 2;
+
+				var url    = 'http://twitter.com/share?text=' + encodeURIComponent(text);
+				var opts   = 'status=1,width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
+
+				window.open(url, 'twitter', opts);
+			});
+
+			// Google+ share button
+			_this.find('[data-easyshare-button="google"]').click(function() {
+				var width  = 500;
+				var height = 400;
+				var left   = ($(window).width() - width) / 2;
+				var top    = ($(window).height() - height) / 2;
+
+				var url    = 'https://plus.google.com/share?url=' + SHARE_URL;
+				var opts   = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
+
+				window.open(url, 'google+', opts);
+			});
+		});
+	}
+};
+
+kyco.easyShare();
