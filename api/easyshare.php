06f550950ab71952dbeb138aa9b66d7eeca92548
Cornelius Weidmann <cornelius@kycosoftware.com>
2015-09-24 00:52:25 +0200

    Now fully supports HTTPS.

--- api/easyshare.php
+++ api/easyshare.php
@@ -1,104 +1,75 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 
-	if (!empty($_SERVER['HTTP_REFERER']))
-	{
-		if (filter_var($_GET['url'], FILTER_VALIDATE_URL) === FALSE)
-		{
-			exit('There is nothing for you here...');
-		}
+if (!empty($_SERVER['HTTP_REFERER'])) {
+  define('SHARED_URL', $_GET['url']);
+  define('FACEBOOK_API_URL', 'https://graph.facebook.com/?ids=');
+  define('TWITTER_API_URL', 'https://urls.api.twitter.com/1/urls/count.json?url=');
+  define('GOOGLE_API_URL', 'https://plusone.google.com/_/+1/fastbutton?url=');
 
-		function get_fb_shares_count($url = FALSE)
-		{
-			define('FACEBOOK_API_URL', 'http://graph.facebook.com/?ids=');
+  if (filter_var(SHARED_URL, FILTER_VALIDATE_URL) === FALSE) {
+    exit('There is nothing for you here... Seems like you supplied an invalid URL...');
+  }
 
-			if (!$url)
-			{
-				$url = ee()->TMPL->fetch_param('url');
-			}
+  function get_fb_shares_count($url) {
+    $file_contents = @file_get_contents(FACEBOOK_API_URL . $url);
+    $response      = json_decode($file_contents, true);
 
-			$file_contents = @file_get_contents(FACEBOOK_API_URL . $url);
-			$response      = json_decode($file_contents, true);
+    if (isset($response[$url]['shares'])) {
+      return intval($response[$url]['shares']);
+    }
+    return 0;
+  }
 
-			if (isset($response[$url]['shares']))
-			{
-				return intval($response[$url]['shares']);
-			}
+  function get_tweet_count($url) {
+    $file_contents = @file_get_contents(TWITTER_API_URL . urlencode($url));
+    $response      = json_decode($file_contents, true);
 
-			return 0;
-		}
+    if (isset($response['count'])) {
+      return intval($response['count']);
+    }
+    return 0;
+  }
 
-		function get_tweet_count($url = FALSE)
-		{
-			define('TWITTER_API_URL', 'http://urls.api.twitter.com/1/urls/count.json?url=');
+  function get_plusone_count($url) {
+    $file_contents = @file_get_contents(GOOGLE_API_URL . urlencode($url));
+    preg_match('/window\.__SSR = {c: ([\d]+)/', $file_contents, $response);
 
-			if (!$url)
-			{
-				$url = ee()->TMPL->fetch_param('url');
-			}
+    if (isset($response[0])) {
+      $total = intval(str_replace('window.__SSR = {c: ', '', $response[0]));
+      return $total;
+    }
+    return 0;
+  }
 
-			$file_contents = @file_get_contents(TWITTER_API_URL . urlencode($url));
-			$response      = json_decode($file_contents, true);
+  $url_parts = parse_url(SHARED_URL);
+  $http_url  = '';
+  $https_url = '';
 
-			if (isset($response['count']))
-			{
-				return intval($response['count']);
-			}
+  if ($url_parts['scheme'] == 'https') {
+    $http_url  = preg_replace("/^https:/i", "http:", SHARED_URL);
+    $https_url = SHARED_URL;
+  } else {
+    $http_url  = SHARED_URL;
+    $https_url = preg_replace("/^http:/i", "https:", SHARED_URL);
+  }
 
-			return 0;
-		}
+  $fb_shares = get_fb_shares_count($http_url) + get_fb_shares_count($https_url);
+  $tweets    = get_tweet_count($http_url) + get_tweet_count($https_url);
+  $plusones  = get_plusone_count($http_url) + get_plusone_count($https_url);
+  $total     = $fb_shares + $tweets + $plusones;
 
-		function get_plusone_count($url = FALSE)
-		{
-			define('GOOGLE_API_URL', 'https://plusone.google.com/_/+1/fastbutton?url=');
+  $response = array(
+    'URL'      => SHARED_URL,
+    'Facebook' => $fb_shares,
+    'Twitter'  => $tweets,
+    'Google'   => $plusones,
+    'Total'    => $total
+  );
 
-			if (!$url)
-			{
-				$url = ee()->TMPL->fetch_param('url');
-			}
-
-			$file_contents = @file_get_contents(GOOGLE_API_URL . urlencode($url));
-
-			preg_match('/window\.__SSR = {c: ([\d]+)/', $file_contents, $response);
-
-			if (isset($response[0]))
-			{
-				$total = intval(str_replace('window.__SSR = {c: ', '', $response[0]));
-
-				return $total;
-			}
-
-			return 0;
-		}
-
-		function get_social_count($url = FALSE)
-		{
-			if (!$url)
-			{
-				$url = ee()->TMPL->fetch_param('url');
-			}
-
-			$fb_shares = get_fb_shares_count($url);
-			$tweets    = get_tweet_count($url);
-			$plusones  = get_plusone_count($url);
-			$total     = $fb_shares + $tweets + $plusones;
-
-			$response = array(
-				'URL'      => $_GET['url'],
-				'Facebook' => $fb_shares,
-				'Twitter'  => $tweets,
-				'Google'   => $plusones,
-				'Total'    => $total
-			);
-
-			return json_encode($response);
-		}
-
-		echo get_social_count($_GET['url']);
-	}
-	else
-	{
-		// Request is not from a valid source...
-		echo 'There is nothing for you here...';
-	}
+  echo json_encode($response);
+} else {
+  // Request is not from a valid source...
+  echo 'There is nothing for you here...';
+}
 ?>
--- dist/jquery.kyco.easyshare.js
+++ dist/jquery.kyco.easyshare.js
@@ -1,16 +1,16 @@
 /***************************************\
 
-	jquery.kyco.easyshare
-	=====================
+  jquery.kyco.easyshare
+  =====================
 
-	Version 1.1.2
+  Version 1.1.3
 
-	Brought to you by
-	http://www.kycosoftware.com
+  Brought to you by
+  https://www.kycosoftware.com
 
-	Copyright 2015 Cornelius Weidmann
+  Copyright 2015 Cornelius Weidmann
 
-	Distributed under the GPL
+  Distributed under the GPL
 
 \***************************************/
 
@@ -19,144 +19,144 @@ var kyco = kyco || {};
 kyco.apiPath = '../api/easyshare.php';
 
 kyco.easyShare = function() {
-	if ($('[data-easyshare]').length > 0) {
-		$('[data-easyshare]').each(function(index, element) {
-			var _this     = $(this);
-			var url       = _this.data('easyshare-url');
-			var SHARE_URL = typeof url === 'undefined' || url === '' ? window.location.href : url;
+  if ($('[data-easyshare]').length > 0) {
+    $('[data-easyshare]').each(function(index, element) {
+      var _this     = $(this);
+      var url       = _this.data('easyshare-url');
+      var SHARE_URL = typeof url === 'undefined' || url === '' ? window.location.href : url;
 
-			var countTotal    = _this.find('[data-easyshare-total-count]');
-			var countFacebook = _this.find('[data-easyshare-button-count="facebook"]');
-			var countTwitter  = _this.find('[data-easyshare-button-count="twitter"]');
-			var countGoogle   = _this.find('[data-easyshare-button-count="google"]');
-			var loader        = _this.find('[data-easyshare-loader]');
+      var countTotal    = _this.find('[data-easyshare-total-count]');
+      var countFacebook = _this.find('[data-easyshare-button-count="facebook"]');
+      var countTwitter  = _this.find('[data-easyshare-button-count="twitter"]');
+      var countGoogle   = _this.find('[data-easyshare-button-count="google"]');
+      var loader        = _this.find('[data-easyshare-loader]');
 
-			// Get share counts for Facebook, Twitter and Google+
-			$.ajax({
-				url: kyco.apiPath,
-				type: 'GET',
-				data: {
-					url: SHARE_URL
-				},
-				dataType: 'json',
-				success: function(response) {
-					countTotal.html(kyco.easyShareApproximate(response.Total));
-					countFacebook.html(kyco.easyShareApproximate(response.Facebook));
-					countTwitter.html(kyco.easyShareApproximate(response.Twitter));
-					countGoogle.html(kyco.easyShareApproximate(response.Google));
+      // Get share counts for Facebook, Twitter and Google+
+      $.ajax({
+        url: kyco.apiPath,
+        type: 'GET',
+        data: {
+          url: SHARE_URL
+        },
+        dataType: 'json',
+        success: function(response) {
+          countTotal.html(kyco.easyShareApproximate(response.Total));
+          countFacebook.html(kyco.easyShareApproximate(response.Facebook));
+          countTwitter.html(kyco.easyShareApproximate(response.Twitter));
+          countGoogle.html(kyco.easyShareApproximate(response.Google));
 
-					loader.fadeOut(500);
-				},
-				error: function() {
-					countTotal.html(0);
-					countFacebook.html(0);
-					countTwitter.html(0);
-					countGoogle.html(0);
+          loader.fadeOut(500);
+        },
+        error: function() {
+          countTotal.html(0);
+          countFacebook.html(0);
+          countTwitter.html(0);
+          countGoogle.html(0);
 
-					loader.fadeOut(500);
-				}
-			});
+          loader.fadeOut(500);
+        }
+      });
 
-			// Facebook share button
-			_this.find('[data-easyshare-button="facebook"]').click(function() {
-				var width  = 500;
-				var height = 400;
-				var left   = ($(window).width() - width) / 2;
-				var top    = ($(window).height() - height) / 2;
+      // Facebook share button
+      _this.find('[data-easyshare-button="facebook"]').click(function() {
+        var width  = 500;
+        var height = 400;
+        var left   = ($(window).width() - width) / 2;
+        var top    = ($(window).height() - height) / 2;
 
-				var url    = 'https://www.facebook.com/sharer/sharer.php?u=' + SHARE_URL;
-				var opts   = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
+        var url    = 'https://www.facebook.com/sharer/sharer.php?u=' + SHARE_URL;
+        var opts   = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
 
-				window.open(url, 'facebook', opts);
-			});
+        window.open(url, 'facebook', opts);
+      });
 
-			// Twitter share button
-			_this.find('[data-easyshare-button="twitter"]').click(function() {
-				var text   = $(this).data('easyshare-tweet-text') || '';
-				var width  = 575;
-				var height = 440;
-				var left   = ($(window).width() - width) / 2;
-				var top    = ($(window).height() - height) / 2;
+      // Twitter share button
+      _this.find('[data-easyshare-button="twitter"]').click(function() {
+        var text   = $(this).data('easyshare-tweet-text') || '';
+        var width  = 575;
+        var height = 440;
+        var left   = ($(window).width() - width) / 2;
+        var top    = ($(window).height() - height) / 2;
 
-				var url    = 'http://twitter.com/share?text=' + encodeURIComponent(text);
-				var opts   = 'status=1,width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
+        var url    = 'https://twitter.com/share?text=' + encodeURIComponent(text);
+        var opts   = 'status=1,width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
 
-				window.open(url, 'twitter', opts);
-			});
+        window.open(url, 'twitter', opts);
+      });
 
-			// Google+ share button
-			_this.find('[data-easyshare-button="google"]').click(function() {
-				var width  = 500;
-				var height = 400;
-				var left   = ($(window).width() - width) / 2;
-				var top    = ($(window).height() - height) / 2;
+      // Google+ share button
+      _this.find('[data-easyshare-button="google"]').click(function() {
+        var width  = 500;
+        var height = 400;
+        var left   = ($(window).width() - width) / 2;
+        var top    = ($(window).height() - height) / 2;
 
-				var url    = 'https://plus.google.com/share?url=' + SHARE_URL;
-				var opts   = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
+        var url    = 'https://plus.google.com/share?url=' + SHARE_URL;
+        var opts   = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left;
 
-				window.open(url, 'google+', opts);
-			});
-		});
-	}
+        window.open(url, 'google+', opts);
+      });
+    });
+  }
 };
 
 /*
-**	kyco.easyShareAddCommas, kyco.easyShareFormatDecimals & kyco.easyShareApproximate
-**	are adapted from https://github.com/nfriedly/approximate-number
-**	Copyright (c) 2014 Nathan Friedly
-**	Licensed under the MIT license
-**	Modified by Cornelius Weidmann
+**  kyco.easyShareAddCommas, kyco.easyShareFormatDecimals & kyco.easyShareApproximate
+**  are adapted from https://github.com/nfriedly/approximate-number
+**  Copyright (c) 2014 Nathan Friedly
+**  Licensed under the MIT license
+**  Modified by Cornelius Weidmann
 */
 kyco.easyShareAddCommas = function(num) {
-	var out    = [];
-	var digits = Math.round(num).toString().split('');
+  var out    = [];
+  var digits = Math.round(num).toString().split('');
 
-	if (num < 1000) {
-		return num.toString();
-	}
+  if (num < 1000) {
+    return num.toString();
+  }
 
-	digits.reverse().forEach(function(digit, i){
-		if (i && i%3 === 0) {
-			out.push(',');
-		}
+  digits.reverse().forEach(function(digit, i){
+    if (i && i%3 === 0) {
+      out.push(',');
+    }
 
-		out.push(digit);
-	});
+    out.push(digit);
+  });
 
-	return out.reverse().join('');
+  return out.reverse().join('');
 };
 
 kyco.easyShareFormatDecimals = function (num, base) {
-	var workingNum = num/base;
+  var workingNum = num/base;
 
-	return workingNum < 10 ? (Math.round(workingNum * 10) / 10) : Math.round(workingNum);
+  return workingNum < 10 ? (Math.round(workingNum * 10) / 10) : Math.round(workingNum);
 };
 
 kyco.easyShareApproximate = function(num) {
-	var negative = num < 0;
-	var numString;
+  var negative = num < 0;
+  var numString;
 
-	if (negative) {
-		num = Math.abs(num);
-	}
+  if (negative) {
+    num = Math.abs(num);
+  }
 
-	if (num < 10000) {
-		numString = kyco.easyShareAddCommas(num);
-	} else if (num < 1000000) {
-		numString =  kyco.easyShareFormatDecimals(num, 1000) + 'k';
-	} else if (num < 1000000000) {
-		numString =  kyco.easyShareFormatDecimals(num, 1000000) + 'm';
-	} else {
-		numString =  kyco.easyShareAddCommas(kyco.easyShareFormatDecimals(num,  1000000000)) + 'b';
-	}
+  if (num < 10000) {
+    numString = kyco.easyShareAddCommas(num);
+  } else if (num < 1000000) {
+    numString =  kyco.easyShareFormatDecimals(num, 1000) + 'k';
+  } else if (num < 1000000000) {
+    numString =  kyco.easyShareFormatDecimals(num, 1000000) + 'm';
+  } else {
+    numString =  kyco.easyShareAddCommas(kyco.easyShareFormatDecimals(num,  1000000000)) + 'b';
+  }
 
-	if (negative) {
-		numString = '-' + numString;
-	}
+  if (negative) {
+    numString = '-' + numString;
+  }
 
-	return numString;
+  return numString;
 };
 
 $(document).ready(function() {
-	kyco.easyShare();
+  kyco.easyShare();
 });
